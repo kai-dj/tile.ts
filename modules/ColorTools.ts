127ef6d4bae74eccdcf8cf50e8234323f8c9506e
kai-dj <kai@kai.dj>
2018-11-25 22:52:32 +0100

    first commit - test with Ammann–Beenker tiling (quarter)

---
+++ modules/ColorTools.ts
@@ -0,0 +1,12 @@
+function randomColorByLetters():string {
+    const letters:string = '0123456789ABCDEF';
+    let color:string = '#';
+    for (let i:number = 0; i < 3; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+export function getRandomColor(): string {
+    return randomColorByLetters();
+};
